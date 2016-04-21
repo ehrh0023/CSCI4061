05425ea5aaab3093ebc75a93756dcbfdedfc81d0
Lord Dennis <ehrh0023@umn.edu>
2016-04-21 13:39:11 -0500

    Rough Init Code; Tried to Fix Makefile

--- Project4/SourceCode/server.c
+++ Project4/SourceCode/server.c
@@ -1,8 +1,6 @@
 /* csci4061 S2016 Assignment 4 
-* section: one_digit_number 
-* date: mm/dd/yy 
-* names: Name of each member of the team (for partners)
-* UMN Internet ID, Student ID (xxxxxxxx, 4444444), (for partners)
+ * Name: Caleb Biasco, Dennis Ehrhardt, Meghan Jonas
+ * X500: biasc007, ehrh0023, jonas050 */
 */
 
 #include <stdio.h>
@@ -21,29 +19,78 @@
 //Structure for queue.
 typedef struct request_queue
 {
-        int             m_socket;
-        char    m_szRequest[MAX_REQUEST_LENGTH];
+	int             m_socket;
+	char    m_szRequest[MAX_REQUEST_LENGTH];
 } request_queue_t;
+
+// Global Variables
+int port;
+int cache_size;
+
+// Buffer Information
+request_queue_t queue[MAX_REQUEST_LENGTH];
+int queue_length;
+int queue_index = 0;
 
 void * dispatch(void * arg)
 {
-        return NULL;
+	return NULL;
 }
 
 void * worker(void * arg)
 {
-        return NULL;
+	return NULL;
 }
 
 int main(int argc, char **argv)
 {
-        //Error check first.
-        if(argc != 6 && argc != 7)
-        {
-                printf("usage: %s port path num_dispatcher num_workers queue_length [cache_size]\n", argv[0]);
-                return -1;
-        }
+	int num_dispatcher = 0;
+	int num_workers = 0;
+	int i;
+	pthread_t dispatchers[MAX_THREADS];
+	pthread_t workers[MAX_THREADS];
+	//Error check first.
+	if(argc != 6 && argc != 7)
+	{
+			printf("usage: %s port path num_dispatcher num_workers queue_length [cache_size]\n", argv[0]);
+			return -1;
+	}
 
-        printf("Call init() first and make a dispather and worker threads\n");
-        return 0;
+	printf("Call init() first and make a dispather and worker threads\n");
+	port = atoi(argv[1]);
+	init(port);
+	
+	num_dispatcher = atoi(argv[3]);
+	num_workers = atoi(argv[4]);
+	
+	queue_length = atoi(argv[5]);
+	cache_size = atoi(argv[6]);
+	
+	// Create Dispatchers
+	for(i = 0; i < num_dispatcher; i++)
+	{
+		if (pthread_create(&dispatchers[i],NULL,dispatch,NULL))
+		  perror("couldn't create thread\n");		
+	}
+	// Create Workers
+	for(i = 0; i < num_workers; i++)
+	{
+		if (pthread_create(&workers[i],NULL,worker,NULL))
+		  perror("couldn't create thread\n");		
+	}
+	
+	// Join Dispatchers
+	for(i = 0; i < num_dispatcher; i++)
+	{
+		if (pthread_join(&dispatchers[i],NULL) != 0)
+			perror("couldn't join thread\n");
+	}
+	// Join Workers
+	for(i = 0; i < num_workers; i++)
+	{
+		if (pthread_join(&workers[i],NULL) != 0)
+			perror("couldn't join thread\n"); 
+	}
+	
+	return 0;
 }
